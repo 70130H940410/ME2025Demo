dbc75d155dd5f3e7e163236e62081a1810bcee7b
70130H940410 <70130h940410@gmail.com>
2025-09-25 15:24:43 +0800

    增加if判斷數字大小，並使用defer

--- HW4_JavaScript1/static/1123103537_js_1.js
+++ HW4_JavaScript1/static/1123103537_js_1.js
@@ -5,3 +5,28 @@ function newAns() {
 let ans=newAns();
 let guessCount=0;
 
+
+const form=document.getElementById("guessForm")
+const input=document.getElementById("guessInput")
+const hint=document.getElementById("hint")
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const n = Number(input.value);
+    guessCount++;
+
+
+    if (n > ans) {
+        alert("太大了，請再試一次。");
+    }
+    else if (n < ans) {
+        alert("太小了，請再試一次。");
+    }
+    else {
+        alert(`恭喜你，猜對了！答案是 ${ans}，你總共猜了 ${guessCount} 次。`);
+        ans = newAns();
+        guessCount = 0;
+    }
+});
+
