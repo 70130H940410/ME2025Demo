63bebbf3bf7b723dc28c04abdf1963eced0095ee
70130H940410 <70130h940410@gmail.com>
2025-09-27 19:57:22 +0800

    處理console.log顯示問題，並且增加margin

--- HW4_JavaScript1/static/112303537_js_2.js
+++ HW4_JavaScript1/static/112303537_js_2.js
@@ -1,22 +1,24 @@
 document.write('<input type="text" id="inputNum"><br>');
 
 for(let i=0;i<=9;i++){
-  document.write('<button onclick="press(\''+i+'\')"> '+i+' </button>');
+  document.write('<button style="margin:5px;" onclick="press(\''+i+'\')"> '+i+' </button>');
   if (i % 3 === 2) document.write("<br>");
 }
 
-document.write('<button onclick="press(\'clear\')">clear</button><br>');
+//筆記:這邊margin加下來後，css跟html寫法需要再複習//
+
+document.write('<button style="margin:5px;" onclick="press(\'clear\')">clear</button><br>');
 
 let ops = ["+", "-", "*", "/", "(", ")", "=","←"];
 for (let op of ops) {
-  document.write('<button onclick="press(\''+op+'\')">' + op+ '</button>');
+  document.write('<button style="margin:5px;" onclick="press(\''+op+'\')">' + op+ '</button>');
 }
 
 //筆記:press(\' '+op+' \') 這樣的寫法就是 → 幫每個按鈕生成一個 onclick 事件，按下去會呼叫 press(某個符號)。//
 
 function press(valueInput){
   const displayOnScreen = document.getElementById("inputNum");
-  console.log(displayOnScreen);
+  
 
   if (valueInput === "clear") {
     displayOnScreen.value = "";
@@ -30,9 +32,11 @@ function press(valueInput){
   else if (valueInput === "←") {
     displayOnScreen.value = displayOnScreen.value.slice(0, -1);
   }
+  //作業外的刪除鍵//
   else{
     displayOnScreen.value = displayOnScreen.value + valueInput;
   }
+  console.log(displayOnScreen.value);
 }
 
   
