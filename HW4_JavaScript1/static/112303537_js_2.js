3bd9789b2531b4dc7bfc8f7d8a0bc46f9c02f7db
70130H940410 <70130h940410@gmail.com>
2025-09-27 19:32:43 +0800

    alert算式與答案(eval)，輸入框由算式變更為答案，clear按鈕會清除輸入框內容

--- HW4_JavaScript1/static/112303537_js_2.js
+++ HW4_JavaScript1/static/112303537_js_2.js
@@ -1,15 +1,15 @@
 document.write('<input type="text" id="inputNum"><br>');
 
 for(let i=0;i<=9;i++){
-  document.write('<button onclick="press(\'  '+i+' \')"> '+i+' </button>');
+  document.write('<button onclick="press(\''+i+'\')"> '+i+' </button>');
   if (i % 3 === 2) document.write("<br>");
 }
 
-document.write('<button onclick="clearNum()">clear</button><br>');
+document.write('<button onclick="press(\'clear\')">clear</button><br>');
 
 let ops = ["+", "-", "*", "/", "(", ")", "="];
 for (let op of ops) {
-  document.write('<button onclick="press(\'  '+op+'  \')">' + op+ '</button>');
+  document.write('<button onclick="press(\''+op+'\')">' + op+ '</button>');
 }
 
 //筆記:press(\' '+op+' \') 這樣的寫法就是 → 幫每個按鈕生成一個 onclick 事件，按下去會呼叫 press(某個符號)。//
@@ -18,5 +18,18 @@ function press(valueInput){
   const displayOnScreen = document.getElementById("inputNum");
   console.log(displayOnScreen);
 
-  displayOnScreen.value = displayOnScreen.value + valueInput;
+  if (valueInput === "clear") {
+    displayOnScreen.value = "";
+  }
+  else if (valueInput === "=") {
+      let ans = eval(displayOnScreen.value);
+      alert(displayOnScreen.value + " = " + ans);
+      displayOnScreen.value = ans;
+    
+  }
+  else{
+    displayOnScreen.value = displayOnScreen.value + valueInput;
+  }
 }
+
+  
