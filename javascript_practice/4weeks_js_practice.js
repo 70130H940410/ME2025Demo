a0c60d360e1f33af95bbfd05c38e627dd8720836
70130H940410 <70130h940410@gmail.com>
2025-09-27 09:38:21 +0800

    學習js:進度funtion,while,for迴圈等等

---
+++ javascript_practice/4weeks_js_practice.js
@@ -0,0 +1,38 @@
+for(let i=0;i<=5;i++){
+    console.log(i)
+}
+
+
+while(0){
+    console.log("")
+}
+
+
+
+
+
+function func(par1,par2){
+    console.log(par1+par2)
+}
+
+func("hello","world")
+
+
+
+
+
+
+
+alert(30+"40.5")
+alert(30+40.5)
+
+alert("hello");
+
+var test = 0;
+alert(typeof(test));
+
+function testing(){
+
+}
+
+let array=[1,2,3]
