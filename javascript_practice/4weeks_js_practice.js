50214699a7f89fba4e1f307794cae4f965a8b152
70130H940410 <70130h940410@gmail.com>
2025-09-27 10:01:10 +0800

    學習js:DATE

--- javascript_practice/4weeks_js_practice.js
+++ javascript_practice/4weeks_js_practice.js
@@ -1,3 +1,46 @@
+const today=new Date();
+console.log(today.getHours());
+console.log(today.toLocaleTimeString());
+
+
+
+
+
+
+
+/////////////////////////////////////////////
+
+switch(3){
+    case 1:
+       console.log(-12); 
+    break;
+    case 3:
+       console.log(-8); 
+    break;
+    default:
+        console.log("default");
+    break;
+}
+
+/////////////////////////////////////////////
+
+let i=1;
+
+if(i>=1){
+    console.log(i*i);
+    i++;
+}
+else if(i==5){
+    console.log("5");
+    i++;
+}
+else{
+    console.log("");
+    i++;
+}
+
+/////////////////////////////////////////////
+
 for(let i=0;i<=5;i++){
     console.log(i)
 }
@@ -8,7 +51,7 @@ while(0){
 }
 
 
-
+/////////////////////////////////////////////
 
 
 function func(par1,par2){
@@ -18,18 +61,18 @@ function func(par1,par2){
 func("hello","world")
 
 
+/////////////////////////////////////////////
 
 
+console.log(30+"40.5")
+console.log(30+40.5)
 
+/////////////////////////////////////////////
 
-
-alert(30+"40.5")
-alert(30+40.5)
-
-alert("hello");
+console.log("hello");
 
 var test = 0;
-alert(typeof(test));
+console.log(typeof(test));
 
 function testing(){
 
