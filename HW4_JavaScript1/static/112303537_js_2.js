47a73760c996c1b4afcd2828cf4be0e908381de4
70130H940410 <70130h940410@gmail.com>
2025-09-27 18:57:11 +0800

    加上clear與其他按鈕

--- HW4_JavaScript1/static/112303537_js_2.js
+++ HW4_JavaScript1/static/112303537_js_2.js
@@ -1,6 +1,17 @@
 document.write('<input type="text" id="inputNum"><br>');
 
-for(let i=1;i<=9;i++){
-  document.write('<button onclick="press"> '+i+' </button>');
-  if (i % 3 === 0) document.write("<br>");
+for(let i=0;i<=9;i++){
+  document.write('<button onclick="press()"> '+i+' </button>');
+  if (i % 3 === 2) document.write("<br>");
+}
+
+document.write('<button onclick="clearNum()">clear</button><br>');
+
+let ops = ["+", "-", "*", "/", "(", ")", "="];
+for (let op of ops) {
+  document.write('<button onclick="press()">' + op + '</button>');
+}
+
+function press(){
+  
 }
