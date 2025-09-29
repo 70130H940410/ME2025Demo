43cf61ba6f4f30f713c718a2a8aa1cebd03d1ab0
70130H940410 <70130h940410@gmail.com>
2025-09-29 09:26:20 +0800

    在js練習檔案中，建立新的資料夾，並開始練習第五周的內容

---
+++ javascript_practice/static/5weeks_js_practice.js
@@ -0,0 +1,6 @@
+const div_byid=document.getElementById('div_1');
+const div_byclassname=document.getElementsByClassName('div_class');
+const div_qry=document.querySelector('.div_class');
+const div_qryA=document.querySelectorAll('.div_class');
+
+div_byid.style.backgroundColor="green"
